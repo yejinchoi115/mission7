c25727ebb669b8aa741e3849f2c8c87335bb98af
yejinchoi115 <yjjjang115@gmail.com>
2022-02-13 22:34:35 -0700

    Initial check-in of module m7

---
+++ m7/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using m7.Models;
+using m7.Models.ViewModels;
+
+namespace m7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController(IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new ProjectsViewModel
+            {
+                Books = repo.Books
+                .OrderBy(p => p.Title)
+                .Skip((pageNum -1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumProjects = repo.Books.Count(),
+                    ProjectsPerPage = pageSize,
+                    Currentpage = pageNum
+                }
+
+            };
+            return View(x);
+        }
+
+
+   
+    }
+}
